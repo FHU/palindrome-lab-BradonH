b725e523919a7bf8c240ee13c74e1c9a76e34d23
BradonH <Bradon.hopper@students.fhu.edu>
2024-02-05 22:48:57 -0600

    update main.py
    finally got good output

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    backwards = []
+    if len(word) <= 1:
+        return False
+    else:
+        for i in range(0,len(word)):
+            backwards.append(word[i])
+        backwards.reverse()
+        backwards = ''.join(backwards)
+        if backwards == word:
+            return True
+        else:
+            return False
+
+
+
 
 #YOUR CODE GOES HERE
+test_word = str(input())
+test_word = test_word.strip()
+segment = ["1", "2", "3"]
+palindrome(test_word) 
+
